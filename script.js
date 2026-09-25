c414073071a95db9f39c3ccef70739cdf064d89b
Varshini <varshini.srini07@gmail.com>
2026-09-24 22:35:23 -0400

    adding base html and css

---
+++ script.js
@@ -0,0 +1,11 @@
+const dialog = document.getElementById("formDialog");
+
+// Function to open the pop-up form as a modal
+function openForm() {
+  dialog.showModal(); 
+}
+
+// Function to close the pop-up form
+function closeForm() {
+  dialog.close();
+}
